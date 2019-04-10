9a3b9cf42a071b103e826c3e7d160636db0334a0
Markuson <el.marcuson@gmail.com>
2019-04-10 17:56:40 +0200

    safe-box started

---
+++ staff/marc-uson/safe-box/safe-box.js
@@ -0,0 +1,19 @@
+'use strict';
+/**
+ * if only is passed the correct password, should return true and show the saved parameter.
+ * Should return false if the pasword is not correct 
+ * 
+ * If a pasword and argument1 is passed, should add argument1 in the safeBox if the password is correct
+ * and throw an Error if the pasword is not correct.
+ * 
+ * if password, argument1 is passed, and changePass is true. if password is correct, should change the 
+ * value of the saved
+ * 
+ * @param {String} password 
+ * @param {*} argument1 
+ * @param {Boolean} changePass 
+ */
+
+function safeBox(password, argument1, changePass){
+    //haz algo
+}
---
+++ staff/marc-uson/safe-box/safe-box.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+describe('safeBox', function(){
+    it('should return true if password is correct and show the secret', function(){
+        
+    });
+
+});
