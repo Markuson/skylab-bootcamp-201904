d1c052e7e37aa65f6b212d9dba6fdfa18ffedcf5
Markuson <el.marcuson@gmail.com>
2019-04-11 09:27:33 +0200

    added safe-box by manu

--- staff/marc-uson/safe-box/safe-box.js
+++ staff/marc-uson/safe-box/safe-box.js
@@ -1,19 +1,32 @@
 'use strict';
-/**
- * if only is passed the correct password, should return true and show the saved parameter.
- * Should return false if the pasword is not correct 
- * 
- * If a pasword and argument1 is passed, should add argument1 in the safeBox if the password is correct
- * and throw an Error if the pasword is not correct.
- * 
- * if password, argument1 is passed, and changePass is true. if password is correct, should change the 
- * value of the saved
- * 
- * @param {String} password 
- * @param {*} argument1 
- * @param {Boolean} changePass 
- */
 
-function safeBox(password, argument1, changePass){
-    //haz algo
-}
+/**
+ * Keeps any secret safe, unnaccessible from any scope where its run.
+ * 
+ * @param {*} password The accessing password.
+ * @param {*} secretOrNewPassword The secret to keep safe or the new password to update.
+ * @param {[boolean]} changePassword If true indicates second argument is a new password and must be updated.
+ */
+var safeBox = (function() {
+    var __password = '123';
+    var __secret;
+
+    function safeBox(password, secretOrNewPassword, changePassword) {
+        if (password === __password) {
+            if (arguments.length === 1) {
+                return __secret;
+            } else if (arguments.length === 2) {
+                __secret = secretOrNewPassword;
+    
+                return true;
+            } else if (arguments.length === 3 && changePassword) {
+                __password = secretOrNewPassword;
+    
+                return true;
+            }
+        } else throw Error('wrong password');
+    }
+
+    return safeBox;
+})();
+
