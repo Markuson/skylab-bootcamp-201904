264aeb0c033034609d5c449fa77c0195f03a6f21
Markuson <el.marcuson@gmail.com>
2019-04-16 12:54:24 +0200

    adjust components folders to new iteration

--- staff/marc-uson/components/8/scripts/i18n.js
+++ staff/marc-uson/components/8/scripts/i18n.js
@@ -34,5 +34,27 @@ var i18n = {
         es: 'Administrador',
         ca: 'Gestor',
         ga: 'Administrador'
+    },
+    signIn: {
+        en: {
+            title: 'Sign in',
+            email: 'E-mail',
+            password: 'Password'
+        },
+        es: {
+            title: 'Iniciar sesión',
+            email: 'E-milio',
+            password: 'Contraseña'
+        },
+        ca: {
+            title: 'Inici de sessió',            
+            email: 'E-mil·li',
+            password: 'Contrasenya'
+        },
+        ga: {
+            title: 'Inicio da sesión',            
+            email: 'E-miliño',
+            password: 'Contrasinal'
+        }
     }
 };
